0b3b9be33d0e9b4299933491e4533714cd8a5683
Maziar <maziar.zirak.80@gmail.com>
2025-07-22 13:44:40 +0330

    Hero Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,12 @@
 function Hero({ title, description }) {
   return (
     <section className="hero">
-      <h1>{title}</h1>
-      <p>{description}</p>
-      {/*  */}
+      <div className="document">
+        <h1>{title}</h1>
+        <p>{description}</p>
+        <button className="cta-btn"><a href="#course">معرفی درس 🎯</a></button>
+      </div>
+      <div className="banner"></div>
     </section>
   );
 }
